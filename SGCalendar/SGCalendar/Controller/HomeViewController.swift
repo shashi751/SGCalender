31cfaa50d5e113c5a1bbd51ac5dfdc8277bf87f8
shashi gupta <shashi.gupta@yusata.com>
2024-02-09 21:50:43 +0530

    Basic Project setup and file structure added

---
+++ SGCalendar/SGCalendar/Controller/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SGCalender
+//
+//  Created by Shashi Gupta on 09/02/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func openCalendar(_ sender: UIButton) {
+        
+    }
+    
+}
+
